2d810fd2d7abcc6aea9cc694c235996e2423ed6f
shivamjadon <jadon4639@gmail.com>
2021-04-30 20:46:02 +0530

    drivers/soc: Introducing Touchboost driver

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -139,7 +139,6 @@ struct events {
 };
 static struct events events_group;
 static struct task_struct *events_notify_thread;
-
 #define LAST_UPDATE_TOL		USEC_PER_MSEC
 
 /* Bitmask to keep track of the workloads being detected */
@@ -188,8 +187,32 @@ static struct input_handler *handler;
 #define MAX_LENGTH_CPU_STRING	256
 
 
+static int touchboost = 0;
 
 /**************************sysfs start********************************/
+
+static int set_touchboost(const char *buf, const struct kernel_param *kp)
+{
+	int val;
+
+ 	if (sscanf(buf, "%d\n", &val) != 1)
+ 		return -EINVAL;
+
+ 	touchboost = val;
+
+	return 0;
+}
+
+ static int get_touchboost(char *buf, const struct kernel_param *kp)
+ {
+ 	return snprintf(buf, PAGE_SIZE, "%d", touchboost);
+ }
+
+static const struct kernel_param_ops param_ops_touchboost = {
+ 	.set = set_touchboost,
+ 	.get = get_touchboost,
+};
+device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
 
 static int set_num_clusters(const char *buf, const struct kernel_param *kp)
 {
@@ -486,7 +509,11 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
-	int ret;
+	int ret; 
+        const char *reset = "0:0 2:0";
+
+ 	if (touchboost == 0)
+ 		cp = reset;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -494,8 +521,11 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	/* CPU:value pair */
 	if (!(ntokens % 2))
 		return -EINVAL;
+if (touchboost == 0)
+ 		cp = reset;
+ 	else
+ 		cp = buf;
 
-	cp = buf;
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
